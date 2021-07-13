f1e81d993ef101a8caa5f801c4fed809985683fa
Nic McDonald <n.mcdonald83@gmail.com>
2021-07-13 14:16:48 -0600

    add ssparse

---
+++ nicspack/packages/ssparse/package.py
@@ -0,0 +1,26 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Ssparse(CMakePackage):
+    """Ssparse is a tool for parsing SuperSim output files."""
+
+    homepage = "https://github.com/ssnetsim/ssparse"
+    git = "https://github.com/ssnetsim/ssparse.git"
+    maintainers = ['nicmcd']
+
+    version('cmake', branch='cmake')
+
+    depends_on('cmake@3.18:', type='build')
+
+    depends_on('libprim')
+    depends_on('libex')
+    depends_on('libmut')
+    depends_on('libstrop')
+    depends_on('libfio')
+    depends_on('zlib')
+    depends_on('tclap')
